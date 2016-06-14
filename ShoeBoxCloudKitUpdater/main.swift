0b730a8da8894577cf8306c307a885136fc5a03f
Jeff Chimney <jeff.chimney@avssystems.ca>
2016-06-14 14:44:55 -0700

    Initial Commit

---
+++ ShoeBoxCloudKitUpdater/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ShoeBoxCloudKitUpdater
+//
+//  Created by Jeff Chimney on 2016-06-14.
+//  Copyright © 2016 Jeff Chimney. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
